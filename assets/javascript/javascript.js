123d79d92f87f8d07aaeb6d91af9a3291a850631
CKillen <cl_killen@hotmail.com>
2018-10-21 12:59:02 -0400

    Finished all classes, refactored
    Added a lot of changes and bug fixes. Added the ability to add
    classes to thumbnails, and to remove classes. set gallery and
    event listeners now work as expected. Can now set custom ids for
    the main image and thumbnailHolder. Fixed a small screen size
    bug. Added a couple helper functions inside of methods to help
    code be a little bit more readable. Fixed a bug where the src
    of stored imgs was give the absolute path and comparing that
    instead of comparing the relative path which is stored.
    
    TODO update README and asked for code to be looked over

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,9 +5,16 @@ const gallery = {
     maxShown: null,
     currentSelected: 0,
     domMainImage: null,
+    thumbnailClasses: [],
+    currentThumbnailClasses: [],
     domThumbnailHolder: null,
     initalize: function(imageArray, thumbnailId, mainImgId){
+
+        //set object for populating thumbnails and main image
+        this.setMainImageId(mainImgId);
+        this.setThumbnailHolderId(thumbnailId);
         
+        //Determine if user passes an array of imgs or if passing a number for random images
         if(!isNaN(imageArray))
         {
             this.makeRandomGallery(imageArray);
@@ -17,109 +24,135 @@ const gallery = {
             this.setGallery(imageArray);
         }
 
-        this.domMainImage = document.getElementById(mainImgId);
-        this.domThumbnailHolder = document.getElementById(thumbnailId);
-
-        gallery.determineMaxThumbnails();
-        gallery.populateGallery();
+        //determine screen size and how many thumbnails should be displayed
+        this.determineMaxThumbnails();
+        //populate the thumbnails and main image
+        this.populateGallery();
         
+        //need to assign this to a varaiable do to addEventListener being outside of the context of the object
+        const galleryObj = this;
+
+        //add event listener for resize of the screen
         addEventListener('resize', function() {
-            gallery.determineMaxThumbnails();
-            gallery.populateThumbnails();
+            galleryObj.determineMaxThumbnails();
+            galleryObj.populateThumbnails();
         });
 
+        //this is a chainable function
         return this;
     },
     determineMaxThumbnails: function(){
+        //helper function needs an object with the value of this
+        const galleryObj = this;
+
+        //determine screen size and how many thumbnails to display
+        //would like to make this user customizable as well possible having an object 
+        // that has { 1200: 9; } then loop through each property to determine the screen 
+        //deimensions/maxthumbnails
         if(document.documentElement.clientWidth > 1200 || window.innerWidth > 1200){
-            if(this.gallery.length > 9)
-            {
-                this.maxShown = 9;
-            }
-            else{
-                this.maxShown = this.gallery.length;
-            }
-            
+            gallerySizeCheck(9);            
         }
         else if(document.documentElement.clientWidth > 900 || window.innerWidth > 900){
-            if(this.gallery.length > 7)
-            {
-                this.maxShown = 7;
-            }
-            else{
-                this.maxShown = this.gallery.length;
-            }
+            gallerySizeCheck(7);
         }
         else if(document.documentElement.clientWidth > 720 || window.innerWidth > 720){
-            if(this.gallery.length > 5)
-            {
-                this.maxShown = 5;
-            }
-            else{
-                this.maxShown = this.gallery.length;
-            }
+            gallerySizeCheck(5);
         }
-        else if(document.documentElement.clientWidth > 530 || window.innerWidth > 530){
-            if(this.gallery.length > 3)
-            {
-                this.maxShown = 3;
-            }
-            else{
-                this.maxShown = this.gallery.length;
-            }
+        else if(document.documentElement.clientWidth > 550 || window.innerWidth > 550){
+            gallerySizeCheck(3);
         }
         else{
             this.maxShown = 0;
         }
+            
+            //helper function to see if gallery is less then max
+            
+            function gallerySizeCheck(maxShownConstant)
+            {
+                if(galleryObj.gallery.length > maxShownConstant)
+                {
+                    galleryObj.maxShown = maxShownConstant;
+                }
+                else{
+                    galleryObj.maxShown = galleryObj.gallery.length;
+                }
+            }
     },
     createThumbnailElemenet: function(galleryIndex){
         domNewImg = document.createElement("img");
+        //add classes
         domNewImg.src = this.gallery[galleryIndex];
-        domNewImg.classList.add("m-2");
+
+        attachClasses(this.thumbnailClasses, domNewImg);
+
         domNewImg.style.width = "75px"
-        if(this.galleryIndex === this.currentSelected)
+
+        if(galleryIndex === this.currentSelected)
         {
-            domNewImg.classList.add("border", "border-danger");
+            //add currentClass
+            attachClasses(this.currentThumbnailClasses, domNewImg);
         }
+
         domGalleryThumbnail.appendChild(domNewImg);
-        
+    
+        //helper function to loop through classes and attach on to new img
+        function attachClasses(classArray, element)
+        {
+            
+            for(let i = 0; i < classArray.length; i++)
+            {
+                element.className += " " + classArray[i];
+            }
+        }
+
+        //need for event listener
         let galleryObj = this;
 
         domNewImg.addEventListener("click" , function()
         {
-            galleryObj.currentSelected = galleryObj.gallery.indexOf(this.src);
-            galleryObj.populateThumbnails();
-            galleryObj.changeMainImage();
+            //this.src returns the absolute path, this gets the src directly from the html which will be exactly what the 
+            //user passes everytime
+            let firstQuote = this.outerHTML.indexOf('"' , this.outerHTML.indexOf('src')) + 1;
+            relativeSrc = this.outerHTML.substring(firstQuote , this.outerHTML.indexOf('"', firstQuote));
+
+            galleryObj.currentSelected = galleryObj.gallery.indexOf(relativeSrc);
+
+            galleryObj.populateGallery();
         })
     },
     populateGallery: function(){
         this.populateThumbnails();
         this.changeMainImage();
 
-        let galleryObj = this;
-
+        //this doesn't return this because it should be used at the end of chains to show changes
     },
     makeRandomGallery: function(numOfPics){
         for(let i = 0; i < numOfPics; i++)
         {
-            
             this.gallery.push("https://picsum.photos/" + randomImageSize(1500) + "x" + randomImageSize(1500) + "?random" + i);
         }
-
-        function randomImageSize(max)
-        {
-            return Math.floor(Math.random() * Math.floor(max));
-        }
+            //helper function to randomize images
+            function randomImageSize(max)
+            {
+                return Math.floor(Math.random() * Math.floor(max));
+            }
     },
-    changeMainImage: function() {
-        let domMainImg = document.getElementById("main-gallery-img");
+    changeMainImage: function(src) {
 
-        domMainImg.src = this.gallery[this.currentSelected];
+        //check if src is given if not skip this and just change image
+        if(src !== undefined && src !== null)
+        {
+            this.currentSelected = this.gallery.indexOf(src);
+        }
+
+        this.domMainImage.src = this.gallery[this.currentSelected];
+
+        return this;
     },
     populateThumbnails: function() {
         this.domThumbnailHolder.innerHTML = "";
 
-        if(this.currentSelected === 0)
+        if(this.currentSelected === 0 || this.gallery.length === this.maxShown)
         {
             for(let i = 0; i < this.maxShown; i++)
             {
@@ -144,7 +177,7 @@ const gallery = {
         {
             for(let i = this.gallery.length - this.maxShown; i < this.gallery.length; i++)
             {
-                this.createThumbnailElemenet(i);
+                 this.createThumbnailElemenet(i);
             }        
         }
         else if(this.currentSelected > 0 && this.currentSelected < this.gallery.length)
@@ -155,30 +188,73 @@ const gallery = {
             }        
         }
     },
-    addThumbnailCss: function(){
+    addThumbnailClass: function(newClass){
+        //push to thumbnail class array
+        this.thumbnailClasses.push(newClass);
+        //repopulate thumbnails to show change
+        this.populateThumbnails();
 
+        return this;
     },
-    removeThumbnailCss: function(){
+    removeThumbnailClass: function(removeClass){
+        //find and remove class from array
+        let indexOfClass = this.thumbnailClasses.indexOf(removeClass);
 
+        if(indexOfClass !== -1)
+        {
+            this.thumbnailClasses.splice(indexOfClass, 1);
+        }
+
+        //repopulate thumbnails to show change
+        this.populateThumbnails();
+
+        return this;
+    },  
+    addCurrentThumbnailClass: function(newClass){
+        //push to currentThumbnailArray
+        this.currentThumbnailClasses.push(newClass);
+
+        //repopulate thumbnails to show change
+        this.populateThumbnails();
+
+        return this;
     },
-    addThumbnailClass: function(){
+    removeCurrentThumbnailClass: function(removeClass){
+        //find and remove class from array
+        let indexOfClass = this.currentThumbnailClasses.indexOf(removeClass);
 
-    },
-    removeThumbnailClass: function(){
+        if(indexOfClass !== -1)
+        {
+            this.currentThumbnailClasses.splice(indexOfClass, 1);
+        }
+        //repopulate thumbnails to show change
+        this.populateThumbnails();
 
+        return this;
     },
     setGallery: function(array){
+        this.gallery = array;
+        this.currentSelected = 0;
 
+        this.determineMaxThumbnails();
+        this.populateGallery();
+        
+        //Make chainable
+        return this;
     },
-    setMainImageId: function(mainImageId){
+    setMainImageId: function(mainImageSrc){
+        this.domMainImage = document.getElementById(mainImageSrc);
 
+        return this;
     },
     setThumbnailHolderId: function(thumbnailId){
+        this.domThumbnailHolder = document.getElementById(thumbnailId);
 
+        return this;
     },
     setButtons: function(leftButtonId, rightButtonId)
     {
-        galleryObj = this;
+        let galleryObj = this;
 
         document.getElementById(leftButtonId).addEventListener("click" ,function(){
             if(galleryObj.currentSelected !== 0)
@@ -197,12 +273,20 @@ const gallery = {
                 galleryObj.changeMainImage();
             }
         });
+
+        return this;
     }
 
     
 }
 
+images = ["assets/images/untitled-5.jpg" , "assets/images/untitled-11.jpg" , "assets/images/untitled-12.jpg" , 
+    "assets/images/untitled-15.jpg" , "assets/images/untitled-16.jpg" , "assets/images/untitled-22.jpg" ,
+    "assets/images/untitled-23.jpg" , "assets/images/untitled-33.jpg" , "assets/images/untitled-35.jpg" ,
+    "assets/images/untitled-37.jpg" , "assets/images/untitled-38.jpg" , "assets/images/untitled-43.jpg" , 
+    "assets/images/untitled-46.jpg"]
 
+gallery.initalize(8, "gallery-thumbnail", "main-gallery-img").setButtons("gallery-left", "gallery-right");
 
-gallery.initalize(100, "gallery-thumbnail", "main-gallery-img").setButtons("gallery-left", "gallery-right");
+gallery.addThumbnailClass("m-2").addCurrentThumbnailClass("border border-danger");
 
